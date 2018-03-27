a69d6179e9032e58a311a7048f64f6ceb9169f99
rendoir <danfil.marques@gmail.com>
2018-03-27 13:25:33 +0100

    Added 2 triggers

--- Artifacts/Database/triggers.sql
+++ Artifacts/Database/triggers.sql
@@ -1,12 +1,28 @@
+-- A message is banned when it exceeds the report limits
 CREATE FUNCTION ban_message() RETURNS TRIGGER AS $$
   BEGIN
     UPDATE Message SET is_banned = TRUE;
-    RETURN new;
+    RETURN NEW;
   END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER ban_message
   BEFORE INSERT OR UPDATE OF num_reports ON Message
   FOR EACH ROW
-    WHEN ( new.num_reports >= 5 + new.score^(1/3) )
+    WHEN ( NEW.num_reports >= 5 + new.score^(1/3) )
     EXECUTE PROCEDURE ban_message();
+
+
+-- Check if the correct answer is an answer to that question
+CREATE FUNCTION check_correct() RETURNS TRIGGER AS $$
+  BEGIN
+    IF NOT EXISTS (SELECT * FROM Answer WHERE NEW.correct_answer = id AND NEW.id = question_id) THEN
+      RAISE EXCEPTION 'An answer can only be marked as correct if it is an answer of the question';
+    END IF;
+    RETURN NEW;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_correct
+  BEFORE INSERT OR UPDATE OF correct_answer ON Question
+  FOR EACH ROW EXECUTE PROCEDURE check_correct();
