1d65d89c192e6769155888d5b12b0c3b2510c9b6
rendoir <up201503822@fe.up.pt>
2018-04-01 16:50:33 +0100

    more insert scripts

---
+++ Artifacts/Database/Populate/category.sql
@@ -0,0 +1,17 @@
+insert into category (name) values ('C');
+insert into category (name) values ('C++');
+insert into category (name) values ('Java');
+insert into category (name) values ('Javascript');
+insert into category (name) values ('SQL');
+insert into category (name) values ('Python');
+insert into category (name) values ('Artificial Intelligence');
+insert into category (name) values ('Computer Graphics');
+insert into category (name) values ('PHP');
+insert into category (name) values ('CSS');
+insert into category (name) values ('HTML');
+insert into category (name) values ('Prolog');
+insert into category (name) values ('GLSL');
+insert into category (name) values ('OpenGL');
+insert into category (name) values ('DirectX');
+insert into category (name) values ('Vulkan');
+insert into category (name) values ('Neural Networks');
