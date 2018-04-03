db39b1d1027be3ca2e6558c9f1a3b0b326fe9acd
AndreFCruz <andrecruz97@gmail.com>
2018-04-03 23:32:15 +0100

    hash indices

--- Artifacts/Database/indices.sql
+++ Artifacts/Database/indices.sql
@@ -2,3 +2,4 @@
 CREATE INDEX unique_lowercase_username ON "user" (lower(username));
 CREATE INDEX unique_lowercase_email ON "user" (lower(email));
 
+-- We do not use hash indices, due to it being actively discouraged on the PostgreSQL documentation
