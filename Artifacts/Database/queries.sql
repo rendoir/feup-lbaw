038b234bf55c4f68069012f20dd09c28ac00e8b6
EdgarACarneiro <up201503784@fe.up.pt>
2018-04-03 23:52:07 +0100

    RIP time

--- Artifacts/Database/queries.sql
+++ Artifacts/Database/queries.sql
@@ -1,3 +1,4 @@
+-- SELECT01
 -- Select all comments of a Message, order by their descending score
 SELECT commentable.id, comment.id, score, is_banned, author, content, creation_time
 FROM commentable, comment, message, message_version
@@ -9,6 +10,7 @@ WHERE
 ORDER BY message.score DESC;
 -- TODO check editions
 
+-- SELECT02
 -- Select the first 25 questions, ordered by descending date of the last edition
 SELECT DISTINCT ON (question.id) question.id, title, correct_answer, score, is_banned, author, content, creation_time
 FROM question, commentable, message, message_version
@@ -19,6 +21,7 @@ WHERE
 ORDER BY question.id, message_version.creation_time DESC
 LIMIT 25;
 
+-- SELECT03
 -- Select the IDs of the 25 questions with most answers (the most discussed questions)
 SELECT question.id, COUNT(answer.question_id) AS num_answers
 FROM question, answer, message
@@ -63,19 +66,22 @@ FROM category
 ORDER BY 
   num_posts DESC;
 
--- For a given category, select all the questions and their contents
-SELECT question_id, title, content, correct_answer, score, is_banned, author
+-- For a given category, select the 25 most recent questions and their contents (and select only those that aren't banned)
+SELECT DISTINCT ON (creation_time, question.id) category.id, question_id, title, content, correct_answer, score, creation_time, is_banned, author
 FROM category, question, question_category, message, message_version
 WHERE
-  category.id = $categoryId AND
+  category.id = 4 AND
   question_category.question_id = question.id AND
   question_category.category_id = category.id AND
   question.id = message.id AND
   message.id = message_version.message_id
+GROUP BY question.id, category.id, question_category.question_id, title, content, correct_answer, score, creation_time, is_banned, author, content
+HAVING
+  is_banned = FALSE
 ORDER BY
-  score DESC
+  message_version.creation_time DESC,
+  question.id
 LIMIT 25;
--- TODO check editions
 
 -- Select all the answers of a given question, from newest to oldest
 SELECT DISTINCT ON (answer.id) answer.id, content, creation_time, is_banned, author
@@ -92,13 +98,79 @@ ORDER BY
   creation_time DESC;
 
 
+-- Select all of a User's questions
+SELECT DISTINCT ON (creation_time, question.id) question.id, title, content, score, creation_time, is_banned
+FROM "user" u, message, message_version, question
+WHERE
+  u.id = 4 AND
+  u.id = message.author AND
+  message.id = question.id AND
+  message.id = message_version.message_id
+GROUP BY
+  question.id, title, content, score, creation_time, is_banned
+ORDER BY
+  creation_time DESC,
+  question.id;
+
+
+-- Select all of a User's answers
+SELECT DISTINCT ON (creation_time, answer.id) answer.id, content, score, creation_time, is_banned
+FROM "user" u, message, message_version, answer
+WHERE
+  u.id = 4 AND
+  u.id = message.author AND
+  message.id = answer.id AND
+  message.id = message_version.message_id
+GROUP BY
+  answer.id, content, score, creation_time, is_banned
+ORDER BY
+  creation_time DESC,
+  answer.id;
+
+-- Select all of a User's comments
+SELECT DISTINCT ON (creation_time, comment.id) comment.id, content, score, creation_time, is_banned
+FROM "user" u, message, message_version, comment
+WHERE
+  u.id = 7 AND
+  u.id = message.author AND
+  message.id = comment.id AND
+  message.id = message_version.message_id
+GROUP BY
+  comment.id, content, score, creation_time, is_banned
+ORDER BY
+  creation_time DESC,
+  comment.id;
+
+-- Select all of a User's correct answers
+
+
+-- Select all of a User's unread notifications
+
+
+-- Select all of a User's badges
+
+
+-- Select a User's profile information
+
+
+-- Select a User's total number of questions
+
+
+-- Select a User's total number of answers
+
+
+-- Select a User's total number of comments
+
+
+-- Select all tags that partially match a given string *
+-- Select all questions whose title partially matches a given string *
 
 
 
+-- ** Most Common Modifications ** --
+-- insert a new message version; insert a new question/answer/comment; update user's bio; inserting a new vote (!!!)
 
-
-
-
+-- There are no deletes, because data rules all.
 
 
 
