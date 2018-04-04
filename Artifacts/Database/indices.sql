d85e7139d56245c351e93e14cf9d2a5e15af5a8b
AndreFCruz <andrecruz97@gmail.com>
2018-04-04 12:31:18 +0100

    rip gin

--- Artifacts/Database/indices.sql
+++ Artifacts/Database/indices.sql
@@ -1,11 +1,20 @@
--- We do not use hash indices, due to it being actively discouraged on the PostgreSQL documentation
+DROP INDEX IF EXISTS comment_commentable CASCADE;
+DROP INDEX IF EXISTS message_version_message CASCADE;
+DROP INDEX IF EXISTS message_author CASCADE;
+DROP INDEX IF EXISTS notification_user CASCADE;
+DROP INDEX IF EXISTS category_name CASCADE;
+DROP INDEX IF EXISTS question_title CASCADE;
+DROP INDEX IF EXISTS unique_lowercase_username CASCADE;
+DROP INDEX IF EXISTS unique_lowercase_email CASCADE;
 
+
+-- We do not use hash indices, due to it being actively discouraged on the PostgreSQL documentation
 CREATE INDEX comment_commentable ON comment USING btree(commentable_id);
 CREATE INDEX message_version_message ON message_version USING btree(message_id);
 CREATE INDEX message_author ON message USING btree(author);
 CREATE INDEX notification_user ON notification USING btree(user_id);
 
-CREATE INDEX tag_name ON tag USING gin(name);
+CREATE INDEX category_name ON category USING gist(to_tsvector('english', name));
 CREATE INDEX question_title ON question USING gist(to_tsvector('english', title));
 
 
