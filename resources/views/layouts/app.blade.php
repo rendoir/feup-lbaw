9dc43f767c2b079c85f4f94f6f8230e05447d3ea
AndreFCruz <andrecruz97@gmail.com>
2018-04-15 16:24:05 +0100

    navbar and configs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'SegFault') }}</title>
 
   <script type="text/javascript">
       // Fix for Firefox autofocus CSS bug
