b42e12a7376e17c6e11efbfdd2660ce17f97f3ce
AndreFCruz <andrecruz97@gmail.com>
2018-04-19 11:10:44 +0100

    search query for questions

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,15 @@
 |
 */
 
+use Illuminate\Http\Request;
+
+
 Route::get('/', function () {
     return redirect('questions');
+});
+
+Route::get('questions', function() {
+    return redirect('questions/recent/0');
 });
 
 // Authentication
@@ -26,33 +33,43 @@ Route::get('about', function() {
     return view('pages/about');
 });
 
-
 Route::get('ask_question', function () {
     return view('pages/ask_question');
 });
 
-//Route::get('questions/{page_num?}', function($page_num = 0) {
-//
-//    $questions = App\Question::all()->forPage($page_num, 25);
-//    $most_voted = App\Question::HighlyVoted()->forPage($page_num, 25)->get();
-//
-//    return view('pages/questions', ['questions' => $questions, 'most_voted' => $most_voted]);
-//});
+// Search questions with string query
+Route::get('questions/', function(Request $request) {
+    $query_string = $request->get('query');
+    $page_num = $request->get('page_num', 0);
+    $questions = App\Question::search($query_string)->forPage($page_num, 25);
 
-Route::get('questions', function() {
-    return redirect('questions/recent/0');
+    return view('pages/questions', ['questions' => $questions, 'type' => 'search']);
 });
 
 Route::get('questions/recent/{page_num}', function($page_num) {
     $questions = App\Question::all()->forPage($page_num, 25);
+    // TODO check ordering
 
     return view('pages/questions', ['questions' => $questions, 'type' => 'recent']);
+});
+
+Route::get('questions/hot/{page_num}', function($page_num) {
+    $questions = App\Question::HighlyVoted()->forPage($page_num, 25);
+    // TODO
+    return view('pages/questions', ['questions' => $questions, 'type' => 'hot']);
 });
 
 Route::get('questions/highly-voted/{page_num}', function($page_num) {
     $questions = App\Question::HighlyVoted()->forPage($page_num, 25);
 
     return view('pages/questions', ['questions' => $questions, 'type' => 'highly-voted']);
+});
+
+Route::get('questions/active/{page_num}', function($page_num) {
+    $questions = App\Question::all()->where('correct_answer', '<>', '')->forPage($page_num, 25);
+    // TODO check ordering -> most recent unanswered questions ?
+
+    return view('pages/questions', ['questions' => $questions, 'type' => 'active']);
 });
 
 Route::get('question/{id}', function($id) {
