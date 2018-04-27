3017aa662468d2eb8d463673c1ed803bf7021715
EdgarACarneiro <up201503784@fe.up.pt>
2018-04-27 22:27:26 +0100

    rip this line

--- public/js/app.js
+++ public/js/app.js
@@ -1816,7 +1816,7 @@ function getPreviousComment(inputNode, previousNode) {
 /* 13 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n}\n^\n      Invalid CSS after \"}\": expected \"}\", was \"\"\n      in /home/edgar/Documents/University/lbaw1763/resources/assets/sass/style.scss (line 489, column 2)\n    at runLoaders (/home/edgar/Documents/University/lbaw1763/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /home/edgar/Documents/University/lbaw1763/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /home/edgar/Documents/University/lbaw1763/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/home/edgar/Documents/University/lbaw1763/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/home/edgar/Documents/University/lbaw1763/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.done [as callback] (/home/edgar/Documents/University/lbaw1763/node_modules/neo-async/async.js:7974:18)\n    at options.error (/home/edgar/Documents/University/lbaw1763/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
