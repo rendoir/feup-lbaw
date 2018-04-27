f4e8d61944e3764f7e194c183ffd401cdad8ce40
AndreFCruz <andrecruz97@gmail.com>
2018-04-27 16:11:51 +0100

    patch work for ask_question, not a good fix

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/', function () {
     return redirect(route('recent_questions'));
 });
 
+Route::get('login', function () {
+    return redirect(route('recent_questions'));
+});
+
 // Authentication
 Route::post('login', 'Auth\LoginController@login')->name('login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
