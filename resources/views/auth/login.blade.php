7c348991ffaf815cda11bc9e2b1a11f8262bc424
AndreFCruz <andrecruz97@gmail.com>
2018-04-30 15:32:50 +0100

    palhaçada

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,23 +5,36 @@
         <div class="container-login100">
             <div class="wrap-login100">
                 <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
-					<span class="login100-form-title p-b-26">
+                    {{ csrf_field() }}
+                    <span class="login100-form-title p-b-26">
 						Welcome
 					</span>
                     <span class="login100-form-title p-b-48">
 						<i class="zmdi zmdi-font"></i>
 					</span>
 
-                    <div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
-                        <input class="input100" type="text" name="email">
+                    <div class="wrap-input100 validate-input {{ $errors->has('email') ? ' has-error' : '' }}" data-validate = "Valid email is: a@b.c">
+                        <input id="email" type="email" class="input100" name="email" value="{{ old('email') }}" required autofocus>
+
+                        @if ($errors->has('email'))
+                        <span class="help-block">
+                        <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                        @endif
                         <span class="focus-input100" data-placeholder="Email"></span>
                     </div>
 
-                    <div class="wrap-input100 validate-input" data-validate="Enter password">
+                    <div class="wrap-input100 validate-input {{ $errors->has('password') ? ' has-error' : '' }}" data-validate="Enter password">
 						<span class="btn-show-pass">
 							<i class="zmdi zmdi-eye"></i>
 						</span>
-                        <input class="input100" type="password" name="pass">
+                        <input id="password" type="password" class="input100" name="password" required>
+
+                        @if ($errors->has('password'))
+                        <span class="help-block">
+                        <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                        @endif
                         <span class="focus-input100" data-placeholder="Password"></span>
                     </div>
 
@@ -39,7 +52,7 @@
 							Don’t have an account?
 						</span>
 
-                        <a class="txt2" href="#">
+                        <a class="txt2" href="{{ route('register') }}" style="hover: none">
                             Sign Up
                         </a>
                     </div>
