d34aab0f0b8f6972cfe216857e10c58dac2cfed0
João Carvalho <joaocarvalho9@gmail.com>
2018-05-29 23:12:46 +0100

    Auto stash before merge of "master" and "origin/master"

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-signin-client_id" content="***REMOVED***.apps.googleusercontent.com">
+    <meta http-equiv="Cache-control" content="public">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
